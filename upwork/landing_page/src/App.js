7bfd19fc1607fc533889f17d57cdbe73da26c367
viacrusis_chris - RD&P Committee <113983506+paranoid-android12@users.noreply.github.com>
2023-09-03 17:10:41 +0800

    Updated main title

--- upwork/landing_page/src/App.js
+++ upwork/landing_page/src/App.js
@@ -5,6 +5,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './components/landingStyle.css';
 
@@ -12,8 +13,8 @@ function App(){
     return(
     <div>
         <Navbar expand="lg" className='mainNav'>
-            <Container>
-                <Navbar.Brand href="#home" className='text-light'>Upwork</Navbar.Brand>
+            <Container className='topNav'>
+                <Navbar.Brand href="#home" className='text-light'>Ligma</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
@@ -25,8 +26,29 @@ function App(){
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-
+        <Navbar expand="lg" className='mainNav'>
+            <Container className='topNav'>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link className='text-light'>Development & IT</Nav.Link>
+                    <Nav.Link className='text-light'>AI Services</Nav.Link>
+                    <Nav.Link className='text-light'>Design & Creative</Nav.Link>
+                    <Nav.Link className='text-light'>Sales & Marketing</Nav.Link>
+                    <Nav.Link className='text-light'>Admin & Customer Support</Nav.Link>
+                    <NavDropdown title="More" id="basic-nav-dropdown" ></NavDropdown>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        
         <Container>
+            <h1 className="mainWordTitle">How work<br/>should work</h1>
+            <p className='underMainWordTitle'>Forget the old rules. You can have the best people.<br/>Right now. Right here.</p>
+            <Button className='rounded-pill'>Get Started</Button>
+        </Container>
+
+        {/* <Container>
             <h1>Browse talent by category</h1>
             <p>Looking for work? Browse jobs</p>
             <Row xs={1} sm={2} lg={4} className="mt-4">
@@ -63,7 +85,7 @@ function App(){
                     </Card>
                 </Col>
             </Row>
-        </Container>
+        </Container> */}
     </div>
     )
 }
